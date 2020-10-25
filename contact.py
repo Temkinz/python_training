28edf97743fa558b8d4e1c4fbecbc273720f90d7
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-10-25 18:21:24 +0300

    added test_add_contact.py and contact.py

---
+++ contact.py
@@ -0,0 +1,26 @@
+class Contact():
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home,
+                 mobile, work, fax, email, home_page, bmonth, byear, bday, aday, amonth, ayear,
+                 address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.home_page = home_page
+        self.bmonth = bmonth
+        self.byear = byear
+        self.bday = bday
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
