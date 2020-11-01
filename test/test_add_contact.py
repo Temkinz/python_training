fd0d6afcf09b53f1d8ccc8f1382cee936eb9b88d
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-11-01 13:32:05 +0300

    added classes GroupHelper and ContactHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_create_contact(app):
     app.session.login(username="admin", password="secret")
-    app.complete_contacts_form(Contact(first_name="Igor", middle_name="Igorevich", last_name="Ivanov",
-                                       nickname="IvanIgor", title="Title", company="CompanyA",
-                                       address="Russia, Moscow", home="Moscow", mobile="8926222222222",
-                                       work="CompanyA", fax="None", email="someemail@qa.com",
-                                       home_page="None", bmonth="August", byear="1988", bday="1",
-                                       aday="1", amonth="February", ayear="1999", address2="Russia, Moscow",
-                                       phone2="+780000000000000", notes="some notes"))
+    app.contact.complete_form(Contact(first_name="Igor", middle_name="Igorevich", last_name="Ivanov",
+                                      nickname="IvanIgor", title="Title", company="CompanyA",
+                                      address="Russia, Moscow", home="Moscow", mobile="8926222222222",
+                                      work="CompanyA", fax="None", email="someemail@qa.com",
+                                      home_page="None", bmonth="August", byear="1988", bday="1",
+                                      aday="1", amonth="February", ayear="1999", address2="Russia, Moscow",
+                                      phone2="+780000000000000", notes="some notes"))
     app.go_to_home_page()
