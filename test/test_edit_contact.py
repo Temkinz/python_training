e01ab2a73c992439a2a3c446a205726e1a0b5aae
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-11-07 20:35:18 +0300

    added preconditions for empty lists of groups and contacts

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,6 +3,14 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Igor", middle_name="Igorevich", last_name="Ivanov",
+                               nickname="IvanIgor", title="Title", company="CompanyA",
+                               address="Russia, Moscow", home="Moscow", mobile="8926222222222",
+                               work="CompanyA", fax="None", email="someemail@qa.com",
+                               home_page="None", bmonth="August", byear="1988", bday="1",
+                               aday="1", amonth="February", ayear="1999", address2="Russia, Moscow",
+                               phone2="+780000000000000", notes="some notes"))
     app.contact.edit_first_contact(Contact(first_name="E_Igor", middle_name="E_Igorevich", last_name="E_Ivanov",
                                            nickname="E_IvanIgor", title="E_Title", company="E_CompanyA",
                                            address="E_Russia, Moscow", home="E_Moscow", mobile="08926222222222",
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Logo"))
     app.group.modify_first_group(Group(name="Edited name", header="Edited header", footer="Edited footer"))
 
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Logo"))
     app.group.modify_first_group(Group(name="New name"))
 
 
 def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Logo"))
     app.group.modify_first_group(Group(header="New header"))
