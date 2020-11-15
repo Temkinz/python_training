3d396a907b6eda1aa665b6e6cc040db20ca18617
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-11-15 13:15:48 +0300

    added comparison of groups

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,13 +1,29 @@
 from model.group import Group
 
-
+"""
 def test_modify_first_group(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="Edited name", header="Edited header", footer="Edited footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+"""
 
 
 def test_modify_first_group_name(app):
-    app.group.modify_first_group(Group(name="New name"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New name")
+    group.group_id = old_groups[0].group_id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
 
 
+"""
 def test_modify_first_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+"""
