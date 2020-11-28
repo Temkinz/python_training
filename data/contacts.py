4c90861dac87cf1cb26ddd1dffe7d41a26ac35ac
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-11-28 22:06:26 +0300

    added constant testdata for contacts

--- data/contacts.py
+++ data/contacts.py
@@ -1,45 +1,18 @@
-import random
-import string
 from model.contact import Contact
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_digits(maxlen):
-    digits = string.digits
-    return "".join([random.choice(digits) for i in range(random.randrange(maxlen))])
-
-
-def random_date():
-    return str(random.randrange(1, 12, 1))
-
-
-def random_year():
-    return str(random.randrange(1900, 2020, 1))
-
-
-def random_email(maxlen):
-    symbols = string.ascii_letters + string.digits
-    email_name = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    return email_name + ".gmail.com"
-
-
-def random_month():
-    months = ["September", "October", "November", "May", "June"]
-    month = random.choice(months)
-    return month
-
-
 testdata = [
-    Contact(first_name=random_string("FN", 10), middle_name=random_string("MN", 5), last_name=random_string("LN", 15),
-            nickname=random_string("N", 10), title=random_string("T", 10), company=random_string("C", 10),
-            address=random_string("C", 20), home_phone=random_digits(11), mobile_phone=random_digits(11),
-            work_phone=random_digits(13), fax=random_digits(9), email=random_email(10),
-            home_page=random_string("HP", 10), address2=random_string("C", 25),
-            secondary_phone=random_digits(12), notes=random_string("C", 25), bday=random_date(),
-            bmonth=random_month(), byear=random_year(), aday=random_date(), amonth=random_month(),
-            ayear=random_year())
+    Contact(first_name="E_Igor", middle_name="E_Igorevich", last_name="E_Ivanov",
+            nickname="E_IvanIgor", title="E_Title", company="E_CompanyA",
+            address="E_Russia, Moscow", home_phone="098981111111111", mobile_phone="08926222222222",
+            work_phone="088888888888888", fax="E_None", email="E_someemail@qa.com",
+            home_page="E_None", bmonth="May", byear="1999", bday="2",
+            aday="2", amonth="March", ayear="1988", address2="E_Russia, Moscow",
+            secondary_phone="+0780000000000000", notes="E_some notes"),
+    Contact(first_name="Igor", middle_name="Igorevich", last_name="Ivanov",
+            nickname="IvanIgor", title="Title", company="CompanyA",
+            address="Russia, Moscow", home_phone="9898000000000", mobile_phone="8926222222222",
+            work_phone="88888888888888", fax="None", email="someemail@qa.com",
+            home_page="None", bmonth="August", byear="1988", bday="1",
+            aday="1", amonth="February", ayear="1999", address2="Russia, Moscow",
+            secondary_phone="+780000000000000", notes="some notes")
 ]
